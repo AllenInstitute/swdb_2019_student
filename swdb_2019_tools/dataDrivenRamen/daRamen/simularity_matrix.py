efa6abf93ec672ef4fcafe0807a2a2622c19597e
Daril Brown II <derbrown@ucsd.edu>
2019-09-03 00:06:34 -0700

    fixed stupid code from the original paper writer

--- swdb_2019_tools/dataDrivenRamen/daRamen/simularity_matrix.py
+++ swdb_2019_tools/dataDrivenRamen/daRamen/simularity_matrix.py
@@ -1,18 +1,47 @@
 """ Calculate the Simularity Matrix """
 
 # Model's Code
-from Distance_calculations import likelihood_distance
+import LLSA_calculations as lvarc
 
 # Python Standard Library
 import numpy as np
+import numpy.ma as ma
 from multiprocessing import Pool, cpu_count
 from functools import partial
+
+
+def compute_master_theta(models, windows_w, tseries_w):
+    master_tseries = []
+    for model in models:
+        worm, window_idx = model
+        window = windows_w[window_idx]
+        t0, tf = window
+        ts = tseries_w[worm][t0:tf]
+        master_tseries.append(ts)
+        master_tseries.append([np.nan] * ts.shape[1])
+    master_tseries = ma.masked_invalid(ma.vstack(master_tseries))
+    master_theta, eps = lvarc.get_theta_masked(master_tseries)
+    return master_theta
+
+
+def likelihood_distance(models, windows_w, tseries_w, thetas_w):
+    master_theta = compute_master_theta(models, windows_w, tseries_w)
+    distances = []
+    for model in models:
+        worm_idx, model_idx = model
+        theta = thetas_w[model_idx]
+        window = windows_w[model_idx]
+        t0, tf = window
+        ts = ma.masked_invalid(tseries_w[worm_idx][t0:tf])
+        theta_here, eps = lvarc.get_theta(ts)
+        distances.append(lvarc.loglik_mvn_masked(theta, ts) - lvarc.loglik_mvn_masked(master_theta, ts))
+    return np.sum(distances)
 
 
 def bad_scope(models, all_windows, all_tseries, all_thetas, indexes):
     index_x, index_y = indexes
     sel_models = models[[index_x, index_y]]
-    distance = likelihood_distance(sel_models, all_windows, all_thetas, all_tseries)  # Calculate Distances
+    distance = likelihood_distance(sel_models, all_windows, all_tseries, all_thetas)  # Calculate Distances
     return index_x, index_y, distance
 
 
@@ -45,7 +74,7 @@ def compute_dissimilarity_matrix(models, all_windows, all_tseries, all_thetas, n
     if n_jobs == 1:
         for index_x, index_y in zip(index_i, index_j):
             sel_models = models[[index_x, index_y]]
-            distance = likelihood_distance(sel_models, all_windows, all_thetas, all_tseries)  # Calculate Distances
+            distance = likelihood_distance(sel_models, all_windows, all_tseries, all_thetas)  # Calculate Distances
             dissimilarity_matrix[index_x, index_y] = distance  # Populate the Matrix
     else:
 
@@ -61,4 +90,3 @@ def compute_dissimilarity_matrix(models, all_windows, all_tseries, all_thetas, n
             dissimilarity_matrix[index_x, index_y] = distance  # Populate the Matrix
 
     return dissimilarity_matrix
-
