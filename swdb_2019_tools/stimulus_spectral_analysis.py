315d62ca763a6eb4efd87c20f715fc5daa727dba
erss <erss@bu.edu>
2019-09-05 04:02:16 +0000

    Fixed bug in get_stim_ids_variable function which gets stim IDs for all stimulus configurations

--- swdb_2019_tools/stimulus_spectral_analysis.py
+++ swdb_2019_tools/stimulus_spectral_analysis.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 from mtspec import mtspec
 from mtspec.util import _load_mtdata
 from functools import reduce
+from IPython.core.debugger import set_trace
+
 '''
-OSTH_MULTITAPER_SPECTRUM requires installation of mtspec package. For installation, 
+PSTH_MULTITAPER_SPECTRUM requires installation of mtspec package. For installation, 
 follow instructions on https://krischer.github.io/mtspec/ 
 '''
 def psth_multitaper_spectrum(cache,session_id,unit_ids, stimulus_presentation_id, W,time_step = 1/100, 
@@ -19,6 +21,7 @@ def psth_multitaper_spectrum(cache,session_id,unit_ids, stimulus_presentation_id
     
     
     INPUTS:
+        cache                         = Neuropixels data cache                        
         session_id                    = String, session_id of mouse
         unit_ids                      = Array, unit_ids
         stimulus_presentation_id      = String, simulus presentation name: e.g.'driftings_gratings' or 'spontaneous'..
@@ -41,6 +44,7 @@ def psth_multitaper_spectrum(cache,session_id,unit_ids, stimulus_presentation_id
     session = cache.get_session_data(session_id)
     stim_table = session.get_presentations_for_stimulus(stimulus_presentation_id)
     stim_ids = get_stimulus_ids(stim_table,stimulus_presentation_id,orientation,temporal_frequency,phase,spatial_frequency)
+    
     # Set power spectrum parameters
     duration = stim_table.duration.iloc[0]
     TW = int(duration * W)
@@ -49,7 +53,6 @@ def psth_multitaper_spectrum(cache,session_id,unit_ids, stimulus_presentation_id
     
     # Compute PSTH
     time_domain = np.arange(0,duration+time_step, time_step)
-
     histograms = session.presentationwise_spike_counts(bin_edges=time_domain, 
                                                    stimulus_presentation_ids=stim_ids, 
                                                    unit_ids=unit_ids)
@@ -121,10 +124,12 @@ def get_stimulus_ids(stim_table,stimulus_presentation_id,orientation=None,tempor
             stim_ids = get_stim_ids_variable(stim_table.orientation,[orientation])  
     ##### ------------------DRIFTING GRATINGS -------------------------------
     elif stimulus_presentation_id == 'drifting_gratings':
+        
         if temporal_frequency is None:
             stim_ids1 = get_stim_ids_variable(stim_table.temporal_frequency,[1.0, 8.0, 15.0, 2.0, 4.0])
-        else:
+        else:  
             stim_ids1 = get_stim_ids_variable(stim_table.temporal_frequency,[temporal_frequency])
+
             
         if orientation is None:
             stim_ids2 = get_stim_ids_variable(stim_table.orientation,[90.0, 0.0, 135.0, 315.0, 225.0, 180.0, 270.0, 45.0])
@@ -171,8 +176,9 @@ def get_stim_ids_variable(stim_table_key,values):
     For example, if you want all orientations that are at 90 and 270 degrees:
        Run this line: get_stim_ids_variable(stim_table.orientation,[90,270])
     '''
-    stim_ids = stim_table_key.isin(values).index
-
+  
+    stim_table_boolean = stim_table_key.isin(values)
+    stim_ids = stim_table_key.loc[stim_table_boolean==True].index
     return stim_ids
 
 def get_good_units(session_units,SNR = 1,ISIV = 0.5, structure_acronym = None):
@@ -258,5 +264,56 @@ def get_good_units_df(session_units,SNR = 1,ISIV = 0.5, structure_acronym = None
         
     return unit_ids_df, fast_spiking_unit_df, regular_spiking_unit_df
 
+def psth_average(cache,session_id,unit_ids, stimulus_presentation_id, time_step = 1/100, 
+                             temporal_frequency = None,orientation=None,phase=None,
+                             spatial_frequency=None,show=True):
+    '''
+    PSTH_MULTITAPER_SPECTRUM computes the power spectrum for the peristimulus time histogram (PSTH) of an experiment 
+    for a given mouse  using the multitaper method.  The PSTH is averaged over units and for each presentation of 
+    the stimulus. The number of tapers is calculated using the recommended rule of thumb, 2 * T * W - 1, 
+    where T is the duration of the stimulus presentation and W is the frequency resolution.  95% 
+    confidence intervals are computed by using the jackknife method . 
+    
+    
+    INPUTS:
+        cache                         = Neuropixels data cache                        
+        session_id                    = String, session_id of mouse
+        unit_ids                      = Array, unit_ids
+        stimulus_presentation_id      = String, simulus presentation name: e.g.'driftings_gratings' or 'spontaneous'..
+        W                             = desired frequency resolution
+        time_step                     = bin size for computing PSTH (default is 1/100)
+        temporal_frequency (optional) = If not included, all possible temporal frequencies are included.
+        orientation (optional)        = If not included, all possible orientations are included.
+        phase (optional)              = If not included, all possible phases are included.
+        spatial_frequency (optional)  = If not included, all possible spatial_frequencies are included.
+        show                          = Boolean, signals whether to plot (default is True)
+    
+    RETURNS: 
+        Pxx              = np array power up until Nyquist frequency
+        f                = np array corresponding frequency axis
+        jackknife        = np array corresponding error bars
+        number_of_tapers = number of tapers used to computer the power spectrum
+    
+    '''
+    # Get session and stimulus information
+    session = cache.get_session_data(session_id)
+    stim_table = session.get_presentations_for_stimulus(stimulus_presentation_id)
+    stim_ids = get_stimulus_ids(stim_table,stimulus_presentation_id,orientation,temporal_frequency,phase,spatial_frequency)
+    
+    # Compute PSTH
+    duration = stim_table.duration.iloc[0]
+    time_domain = np.arange(0,duration+time_step, time_step)
+    histograms = session.presentationwise_spike_counts(bin_edges=time_domain, 
+                                                   stimulus_presentation_ids=stim_ids, 
+                                                   unit_ids=unit_ids)
+    
+    mean_histograms = histograms.mean(dim="stimulus_presentation_id")
+    psth_trials = mean_histograms.values/time_step
+    
+    # To get firing rate
+    psth_trials_mean = psth_trials.mean(1)
+    
+    return psth_trials_mean, psth_trials, time_domain
+    
 
 
